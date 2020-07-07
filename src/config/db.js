9805c3b89044b6ae0c834c12276409070e913ff3
Choibumsu <bumsu0211@gmail.com>
2020-07-07 17:13:52 +0900

    [#20] fix(db) - levelDB 저장 폴더명 변경 및 gitignore에 등록

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const level = require('level')
 const subLevel = require('level-sublevel')
 
-const db = subLevel(level('./db.js', { valueEncoding: 'json' }))
+const db = subLevel(level('./db', { valueEncoding: 'json' }))
 
 const userDB = db.sublevel('users')
 
