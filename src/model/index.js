cee07421a8a3ef723653d7a8cc0d191ce1eb9b74
Choibumsu <bumsu0211@gmail.com>
2020-07-07 17:51:15 +0900

    [#20] feat(login) - db에서 특정 유저를 반환하는 함수 작성
    getUserOne : id를 키로 가지는 유저를 반환, 없으면 에러를 출력하고 false 반환

--- src/model/index.js
+++ src/model/index.js
@@ -1,1 +1,12 @@
-// 임시
+const { db } = require('../config/db')
+
+exports.getUserOne = async (id) => {
+  try {
+    const user = await db.get(id)
+
+    return user
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
