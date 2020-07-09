840fb3798f19a7fd35576b5a9005911074cbe8a2
Choibumsu <bumsu0211@gmail.com>
2020-07-09 15:20:15 +0900

    [#38] feat(login) - 로그인 인풋 데이터 검증 함수 구현
    LoginPage 객체 생성시, woowa-btn에 sendLoginRequest 함수 연결
    sendLoginRequest : 아이디, 비밀번호 인풋 태그 값을 검증하고 에러가 있으면 첫번째 에러 메세지를 error-message dom객체에 넣음
    validateInput : 에러의 종류를 반환

---
+++ public/javascripts/pages/LoginPage.js
@@ -0,0 +1,83 @@
+import { TAG_NAME, CLASS_NAME } from '../utils/constants.js'
+import Button from '../components/common/Button.js'
+
+const ERROR_CASES = {
+  id: {
+    empty: '아이디를 입력해주세요.',
+    lengthOver: '아이디는 100자를 넘을 수 없습니다.',
+  },
+  password: {
+    empty: '비밀번호를 입력해주세요.',
+    lengthOver: '비밀번호는 100자를 넘을 수 없습니다.',
+  },
+}
+
+export default function LoginPage(props) {
+  if (new.target !== LoginPage) {
+    return new LoginPage()
+  }
+
+  const { loginFormSelector } = props
+
+  this.init = () => {
+    this.$loginForm = document.querySelector(loginFormSelector)
+    this.$errorNode = this.$loginForm.querySelector('.error-message')
+
+    new Button({
+      selector: '.woowa-btn',
+      onClickHandler: () => this.sendLoginRequest(),
+    })
+  }
+
+  this.sendLoginRequest = () => {
+    const $inputs = Array.from(
+      this.$loginForm.querySelectorAll('input[name=id], input[name=password]')
+    )
+
+    const errorMessages = $inputs.reduce((errorList, $input) => {
+      const result = validateInput($input.value)
+
+      if (result.isError) {
+        errorList.push(ERROR_CASES[$input.name][result.code])
+      }
+      return errorList
+    }, [])
+
+    if (errorMessages.length > 0) {
+      this.$loginForm.classList.add(CLASS_NAME.ERROR_CLASS)
+      this.$errorNode.innerHTML = errorMessages[0]
+    }
+
+    //call login api
+  }
+
+  this.init()
+}
+
+function validateInput(value) {
+  if (value.length > 100) {
+    return {
+      isError: true,
+      code: 'lengthOver',
+    }
+  }
+
+  if (value === '') {
+    return {
+      isError: true,
+      code: 'empty',
+    }
+  }
+
+  return {
+    isError: false,
+  }
+}
+
+try {
+  new LoginPage({
+    loginFormSelector: '.login-form',
+  })
+} catch (e) {
+  console.error(e)
+}
